f3b291684aa35bd3dbb56c96887edd2c3adba58f
38068 <38068@SUPERYOGA>
2022-04-13 20:46:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace BlackJack
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; // won't see ♠♥♣♦ without it
+
+            Console.WriteLine("Welcome to BlackJack♠♥♣♦");
+            Console.WriteLine("Press <Enter> to start");
+            Console.ReadKey();
+            GameStart();
+        }
+    }
+}
