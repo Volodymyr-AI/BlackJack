1be1284860d98cd1c4f6786964f7f194845243c9
Volodymyr <alternativeqoob@gmail.com>
2022-04-14 19:33:35 +0300

    created Card class, cystomized cards and implemented logic

--- Card.cs
+++ Card.cs
@@ -4,9 +4,110 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static BlackJack.Suit;
+using static BlackJack.Face;
+
 namespace BlackJack
 {
-    internal class Card
+
+    public enum Suit
     {
+        Hearts,
+        Spades,
+        Clubs,
+        Diamonds
+    }
+
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+        public int Value { get; set; }
+        public char Symbol { get; }
+
+        //initialize value and suit symbol
+
+        public Card(Suit suit, Face face)
+        {
+            Suit = suit;
+            Face = face;
+
+            switch (Suit)
+            {
+                case Clubs:
+                    Symbol = '♣';
+                    break;
+                case Spades:
+                    Symbol = '♠';
+                    break;
+                case Diamonds:
+                    Symbol = '♦';
+                    break;
+                case Hearts:
+                    Symbol = '♥';
+                    break;
+            }
+            switch (Face)
+            {
+                case Ten:
+                case Jack:
+                case Queen:
+                case King:
+                    Value = 10;
+                    break;
+                case Ace:
+                    Value = 11;
+                    break;
+                default:
+                    Value = (int)Face + 1;
+                    break;
+            }
+        }
+
+        //print out description of card, Ace(soft == 1 or Hard == 11)
+        public void WriteDescription()
+        {
+            if (Suit == Suit.Diamonds || Suit == Suit.Hearts)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                
+            }
+             else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if(Face == Ace)
+            {
+                if(Value == 11)
+                {
+                    Console.WriteLine(Symbol + " Soft" + Face + " of " + Suit);
+                }
+                else
+                {
+                    Console.WriteLine(Symbol + " Hard" + Face + " of " + Suit);
+                }
+            }
+            else
+            {
+                Console.WriteLine(Symbol + " " + Face + " of " + Suit);
+            }
+            Casino.ResetColor();
+        }
     }
 }
