49661fe4d78e71d09c089ba5365cd89af2dbc21b
Volodymyr <alternativeqoob@gmail.com>
2022-04-14 18:48:32 +0300

    created Deck class and implemented logic

--- Deck.cs
+++ Deck.cs
@@ -20,6 +20,60 @@ namespace BlackJack
         public List<Card> GetColdDeck()
         {
             List<Card> coldDeck = new List<Card>();
+
+            for(int i = 0; i < 13; i++)
+            {
+                for(int j = i; j < 4; j++)
+                {
+                    coldDeck.Add(new Card((Suit)j, (Face)i));
+                }
+            }
+            return coldDeck;
+        }
+
+        //remove top 2 card of deck and turn into list
+        public List<Card> Dealhand()
+        {
+            List<Card> hand = new List<Card>();
+            hand.Add(cards[0]);
+            hand.Add(cards[1]);
+            //remove added to hand cards
+            cards.RemoveRange(0, 2);
+
+            return hand;
+        }
+
+        //pick top card and remove it from deck
+
+    public Card DrawCard()
+        {
+            Card card = cards[0];
+            cards.Remove(card);
+
+            return card;
+        }
+
+        //randomize the order of the cards in the deck
+        public void Shuffle()
+        {
+            Random rng = new Random();
+
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n+1);
+                Card card = cards[k];
+                cards[k] = cards[n];
+                cards[n] = card;
+            }
+        }
+
+        //replace the deck with a cold deck and then shuffle it
+        public void Initialize()
+        {
+            cards = GetColdDeck();
+            Shuffle();
         }
     }
 }
